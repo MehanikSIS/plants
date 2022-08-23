08a818f3cef5806782ede3ce351ff01cfcb02f84
Mehanik <Ivanforsag@list.ru>
2022-08-23 18:33:23 +0300

    plants_03.ignore_files

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -7,25 +7,29 @@ import xml.ParsingXML;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.List;
+import java.util.Properties;
 
 
 public class DAO {
 
     public static void main(String[] args) throws ParserConfigurationException, SAXException {
-        Connection c;
-        Statement stmt;
+        Properties prop = new Properties();
         try {
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/plant", "postgres", "root");
+            InputStream input = new FileInputStream("src/main/resources/config.properties");
+            prop.load(input);
+            Connection connection = DriverManager.getConnection(prop.getProperty("urlDataBase"), prop.getProperty("user"), prop.getProperty("password"));
+            Statement stmt = connection.createStatement();
             System.out.println("Opened database successfully");
             for (String fileName : args) {
                 File file = new File(fileName);
                 ParsingXML parsingXML = new ParsingXML();
                 Catalog catalog = new Catalog();
                 List<Plant> plants = parsingXML.parsing(file, catalog);
-                stmt = c.createStatement();
                 stmt.executeUpdate("INSERT INTO d_cat_catalog (delivery_date,company,uuid) VALUES ('"
                         + catalog.getDate()
                         + "', '"
@@ -46,12 +50,13 @@ public class DAO {
                 }
                 System.out.println(file.getName() + " uploaded to database");
             }
+            stmt.close();
+            connection.close();
+            input.close();
         } catch (SQLException | IOException e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-
         }
-
     }
 }
